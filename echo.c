effd71bbf2ccd49423db17eabe9f37e3aa4751e6
Patrick Murray <pmurray1@stevens.edu>
2015-12-15 17:51:26 -0500

    Bug fixes

--- echo.c
+++ echo.c
@@ -5,28 +5,16 @@ void echo(char** arguments)
 {
 	int i;
 
-	/* Replace $? and $$ */
 	for (i = 1; arguments[i] != NULL; i++) {
-		printf("<%s>\n", arguments[i]);
-/*		if (strcmp(arguments[i], "$?") == 0)
-		{
-			printf("Exit Status");
-			//printf("%i", );
-		}
-		else if (strcmp(arguments[i], "$$") == 0)
-		{
-			printf("Current PID");
-			//printf("%i", );
-		}
-		else
-		{
-			printf("%s", arguments[i]);
-		}
+		printf("%s", arguments[i]);
+		
+		arguments[i] = NULL;
 
 		if (arguments[i+1] != NULL) {
 			printf(" ");
 		}
 	}
-*/
+
+	printf("\n");
 }
 
